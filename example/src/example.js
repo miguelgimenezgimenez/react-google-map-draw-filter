538cb9866b7aa8fae8b28d021d3a3a43cf32e857
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2017-01-14 11:24:46 +0100

    remove image from examples

--- example/src/example.js
+++ example/src/example.js
@@ -42,49 +42,49 @@ class App extends Component {
   render() {
     const markers = [
   			{
-  				icon:'images/beergarden.png',
+  				// icon:'images/beergarden.png',
   				info:'beer garden',
   				title:'hello',
   				latLng:{lat:2.13815342634916,lng:41.39485570794}
   			},
 
   			{
-  				icon:'images/dance_class.png',
+  				// icon:'images/dance_class.png',
   				info:'dance class',
   				latLng:{lat:2.1575260162353516,lng: 41.39586980544921}
   			},
 
   			{
-  				icon:'images/dance_class.png',
+  				// icon:'images/dance_class.png',
   				info:'dance class',
   				latLng:{lat:2.162332534790039 ,lng:41.397801375978204}
   			},
 
   			{
-  				icon:'images/dance_class.png',
+  				// icon:'images/dance_class.png',
   				info:'dance class',
   				latLng:{lat:2.154865264892578 ,lng:41.38576031676253}
   			},
 
   			{
-  				icon:'images/gay-female.png',
+  				// icon:'images/gay-female.png',
   				info:'lesbian party',
   				latLng:{lat:2.142505645751953 ,lng:41.38344199588044}
   			},
   			{
-  				icon:'images/gay-female.png',
+  				// icon:'images/gay-female.png',
   				info:'lesbian party',
   				latLng:
   				{lat:2.1316909790039062,lng: 41.40044109620138}
   			},
   			{
-  				icon:'images/gay-male.png',
+  				// icon:'images/gay-male.png',
   				info:'gay party',
   				latLng:
   				{lat:2.130146026611328 ,lng:41.40308070920773}
   			},
   			{
-  				icon:'images/gay-male.png',
+  				// icon:'images/gay-male.png',
   				info:'gay party',
   				latLng:
   				{lat:2.1413040161132812,lng: 41.40346698504464}
